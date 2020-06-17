f7c373efe0d926ce0af06ae5c6362d1a5147e574
JeremyWylde <63048608+JeremyWylde@users.noreply.github.com>
2020-06-17 05:36:35 +0300

    Updates

---
+++ precache-manifest.eb6249713d3dc9b5e634c7500db24e14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd37b76bb50ad87d03afa0a45aaf34f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "498043c68e215b0cf273",
+    "url": "/static/css/main.948cb07e.chunk.css"
+  },
+  {
+    "revision": "1193c3b7f169a7630fd7",
+    "url": "/static/js/2.868afe3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.868afe3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498043c68e215b0cf273",
+    "url": "/static/js/main.64d09b2b.chunk.js"
+  },
+  {
+    "revision": "19d841a2b6b2f7127ce3",
+    "url": "/static/js/runtime-main.e7700ab4.js"
+  }
+]);
